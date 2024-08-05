7a32d7aebd412cfbae844d35a270821802491c4a
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-05 23:55:36 +0300

    feat: add ApplicationDBContext & Employee classes

---
+++ EntityFrameworkCore/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EntityFrameworkCore
+{
+    public class ApplicationDBContext : DbContext
+    {
+        private static IConfigurationRoot? _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        public static string? ConnectionString = _configuration.GetSection("ConnectionString").Value;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -1,7 +1,17 @@
-﻿internal class Program
+﻿using EntityFrameworkCore;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using (var context = new ApplicationDBContext())
+        {
+            Employee employee = new Employee
+            {
+                Name = "Khaled"
+            };
+            context.Employees.Add(employee);
+            context.SaveChanges();
+        }
     }
 }
